42f36f39130c963baff30cb2e359ba2e2269962a
Baozisoftware <->
2017-04-01 15:25:40 +0800

    2017040100

--- cmd_windows.go
+++ cmd_windows.go
@@ -7,6 +7,8 @@ import (
 	"github.com/pkg/browser"
 	"runtime"
 	"os/exec"
+	"syscall"
+	"unsafe"
 )
 
 func cmd() {
@@ -18,7 +20,7 @@ func openWebUI(hta bool) {
 	if port != 80 {
 		u = fmt.Sprintf("%s:%d", u, port)
 	}
-	if runtime.GOOS == "windows" && hta {
+	if runtime.GOOS == "windows" && hta && checkWin10() {
 		err := exec.Command("mshta", u+"/hta").Start()
 		if err != nil {
 			browser.OpenURL(u)
@@ -26,4 +28,31 @@ func openWebUI(hta bool) {
 	} else {
 		browser.OpenURL(u)
 	}
+}
+
+func checkWin10() (ret bool) {
+	defer func() {
+		if recover() != nil {
+			ret = false
+		}
+	}()
+	type OSVERSIONINFOW struct {
+		dwOSVersionInfoSize uint32
+		dwMajorVersion      uint32
+		dwMinorVersion      uint32
+		dwBuildNumber       uint32
+		dwPlatformId        uint32
+		szCSDVersion        [128]byte
+	}
+	var v OSVERSIONINFOW
+	p := uintptr(unsafe.Pointer(&v))
+	kernel32 := syscall.NewLazyDLL("ntdll.dll")
+	c := kernel32.NewProc("RtlGetVersion")
+	r, _, _ := c.Call(p)
+	if r == 0 {
+		ret = v.dwMajorVersion == 10
+	} else {
+		ret = false
+	}
+	return
 }
